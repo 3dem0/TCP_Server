32c653fe01321be06ca4c5ffa0f3a7c1d39a8836
3dem0 <demiansferrari@gmail.com>
2021-03-11 17:00:51 -0300

    Repositorio form
    Recibe TCP

---
+++ Test_TPC_Server/Test_TPC_Server/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Test_TPC_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.botonStartServer = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.consola_texto = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.conectar_serie = new System.Windows.Forms.Button();
+            this.baudRate = new System.Windows.Forms.TextBox();
+            this.consolaSerie = new System.Windows.Forms.RichTextBox();
+            this.consolaSerieLectura = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // botonStartServer
+            // 
+            this.botonStartServer.Location = new System.Drawing.Point(12, 12);
+            this.botonStartServer.Name = "botonStartServer";
+            this.botonStartServer.Size = new System.Drawing.Size(75, 23);
+            this.botonStartServer.TabIndex = 0;
+            this.botonStartServer.Text = "Start Server";
+            this.botonStartServer.UseVisualStyleBackColor = true;
+            this.botonStartServer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // consola_texto
+            // 
+            this.consola_texto.Location = new System.Drawing.Point(12, 196);
+            this.consola_texto.Name = "consola_texto";
+            this.consola_texto.Size = new System.Drawing.Size(417, 242);
+            this.consola_texto.TabIndex = 1;
+            this.consola_texto.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(477, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(299, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // conectar_serie
+            // 
+            this.conectar_serie.Location = new System.Drawing.Point(477, 12);
+            this.conectar_serie.Name = "conectar_serie";
+            this.conectar_serie.Size = new System.Drawing.Size(161, 23);
+            this.conectar_serie.TabIndex = 3;
+            this.conectar_serie.Text = "Conectar Serie";
+            this.conectar_serie.UseVisualStyleBackColor = true;
+            this.conectar_serie.Click += new System.EventHandler(this.conectar_serie_Click);
+            // 
+            // baudRate
+            // 
+            this.baudRate.Location = new System.Drawing.Point(477, 74);
+            this.baudRate.Name = "baudRate";
+            this.baudRate.Size = new System.Drawing.Size(299, 20);
+            this.baudRate.TabIndex = 4;
+            this.baudRate.Text = "9600";
+            // 
+            // consolaSerie
+            // 
+            this.consolaSerie.Location = new System.Drawing.Point(452, 196);
+            this.consolaSerie.Name = "consolaSerie";
+            this.consolaSerie.Size = new System.Drawing.Size(417, 108);
+            this.consolaSerie.TabIndex = 5;
+            this.consolaSerie.Text = "";
+            // 
+            // consolaSerieLectura
+            // 
+            this.consolaSerieLectura.Location = new System.Drawing.Point(452, 310);
+            this.consolaSerieLectura.Name = "consolaSerieLectura";
+            this.consolaSerieLectura.Size = new System.Drawing.Size(417, 128);
+            this.consolaSerieLectura.TabIndex = 6;
+            this.consolaSerieLectura.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 450);
+            this.Controls.Add(this.consolaSerieLectura);
+            this.Controls.Add(this.consolaSerie);
+            this.Controls.Add(this.baudRate);
+            this.Controls.Add(this.conectar_serie);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.consola_texto);
+            this.Controls.Add(this.botonStartServer);
+            this.Name = "Form1";
+            this.Text = "Servidor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonStartServer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox consola_texto;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button conectar_serie;
+        private System.Windows.Forms.TextBox baudRate;
+        private System.Windows.Forms.RichTextBox consolaSerie;
+        private System.Windows.Forms.RichTextBox consolaSerieLectura;
+    }
+}
+
---
+++ Test_TPC_Server/Test_TPC_Server/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO.Ports;
+using System.Threading;
+namespace Test_TPC_Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] puertos = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(puertos);
+        }
+
+        TcpListener server = null;
+
+        System.IO.Ports.SerialPort ArduinoPort;
+
+        Int32 port = 13000;
+
+        String codigoVerificacion = "ABD123";
+        String CodigoRecibido = "";
+        int Servo = 1;
+        int Angulo = 90;
+        String CadenaRecibida;
+        Boolean ServidorOn = false;
+
+        String puerto;
+        String baudRateCom;
+        String cadena = null;
+        Boolean conectadoOk = false;
+        int count = 0;
+        int Encabezado = 255;
+        int Cierre = 254;
+
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        
+        
+        System.Threading.Thread t;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new TcpListener(port);
+            // TcpListener server = new TcpListener(port);
+            server.Start();
+
+            /*
+            t = new System.Threading.Thread(recibe);
+            t.Start();
+            */
+
+        }
+
+
+
+        public void recibe()
+        {
+            if (ServidorOn == true)
+            {
+                try
+                {
+
+                    // Buffer for reading data
+                    Byte[] bytes = new Byte[262144];
+                    String data = null;
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    //Console.WriteLine("Connected!");
+                    data = null;
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+                    int i;
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        CadenaRecibida = data;
+                        string phrase = CadenaRecibida;
+                        string[] words = phrase.Split(',');
+                        int contadorSeparador = 0;
+                        foreach (var word in words)
+                        {
+                            if (contadorSeparador == 0)
+                            {
+                                CodigoRecibido = $"{word}";
+                            }
+                            if (contadorSeparador == 1)
+                            {
+                                Servo = Int32.Parse($"{word}");
+                            }
+                            if (contadorSeparador == 2)
+                            {
+                                Angulo = Int32.Parse($"{word}");
+                                contadorSeparador = 0;
+                            }
+                            contadorSeparador++;
+                        }
+
+
+                            consola_texto.Text = consola_texto.Text + "\n" + CodigoRecibido;
+                            consola_texto.Text = consola_texto.Text + "\n" + Servo;
+                            consola_texto.Text = consola_texto.Text + "\n" + Angulo;
+
+                        /*
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        */
+                        // Send back a response.
+                        //stream.Write(msg, 0, msg.Length);
+                        //Console.WriteLine("Sent: {0}", data);
+
+                        if (conectadoOk == true)
+                        {
+                            byte[] EncabezadoByte = { Convert.ToByte(Encabezado) };
+                            byte[] ServoByte = { Convert.ToByte(Servo) };
+                            byte[] AnguloByte = { Convert.ToByte(Angulo) };
+                            byte[] CierreByte = { Convert.ToByte(Cierre) };
+
+                            ArduinoPort.Write(EncabezadoByte, 0, EncabezadoByte.Length);
+                            ArduinoPort.Write(ServoByte, 0, ServoByte.Length);
+                            ArduinoPort.Write(AnguloByte, 0, AnguloByte.Length);
+                            ArduinoPort.Write(CierreByte, 0, CierreByte.Length);
+                        }
+                    }
+                }
+
+                catch
+                {
+                    //Console.WriteLine("SocketException: {0}", e);
+                }
+                finally
+                {
+                    // Stop listening for new clients.
+                      server.Stop();
+                }
+
+
+            }
+
+            //return true;
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            recibe();
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServidorOn = true;
+        }
+
+        private void conectar_serie_Click(object sender, EventArgs e)
+        {
+            conectar();
+        }
+
+        private void conectar()
+        {
+            //crear Serial Port
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = puerto;  //sustituir por vuestro 
+            ArduinoPort.BaudRate = Int32.Parse(baudRate.Text);
+            ArduinoPort.Open();
+
+            if (ArduinoPort.IsOpen)
+            {
+                MessageBox.Show("Conectado");
+                conectadoOk = true;
+            }
+            else
+            {
+                MessageBox.Show("Desconectado");
+                conectadoOk = false;
+            }
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            puerto = comboBox1.SelectedItem.ToString();
+        }
+    }
+}
